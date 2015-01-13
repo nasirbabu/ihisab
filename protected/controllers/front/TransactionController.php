a20eed7b29e24c467db15037d52a4fc37f97f8ad
Saidur Rahman <saidurwd@gmail.com>
2015-01-13 11:48:12 +0600

    Protected 1st commit

---
+++ protected/controllers/front/TransactionController.php
@@ -0,0 +1,287 @@
+<?php
+
+class TransactionController extends Controller {
+
+    /**
+     * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
+     * using two-column layout. See 'protected/views/layouts/column2.php'.
+     */
+    public $layout = '//layouts/column2';
+
+    /**
+     * @return array action filters
+     */
+    public function filters() {
+        return array(
+            'accessControl', // perform access control for CRUD operations
+            'postOnly + delete', // we only allow deletion via POST request
+        );
+    }
+
+    /**
+     * Specifies the access control rules.
+     * This method is used by the 'accessControl' filter.
+     * @return array access control rules
+     */
+    public function accessRules() {
+        return array(
+            array('allow', // allow all users to perform 'index' and 'view' actions
+                'actions' => array('index', 'view'),
+                'users' => array('*'),
+            ),
+            array('allow', // allow authenticated user to perform 'create' and 'update' actions
+                'actions' => array('index', 'view', 'create', 'single', 'multiple', 'update', 'admin', 'delete'),
+                'users' => array('@'),
+            ),
+            array('allow', // allow admin user to perform 'admin' and 'delete' actions
+                'actions' => array('admin', 'delete'),
+                'users' => array('admin'),
+            ),
+            array('deny', // deny all users
+                'users' => array('*'),
+            ),
+        );
+    }
+
+    public function actionCreate() {
+        $model = new Transaction;
+        $this->render('create', array(
+            'model' => $model,
+        ));
+    }
+
+    /**
+     * Displays a particular model.
+     * @param integer $id the ID of the model to be displayed
+     */
+    public function actionView($id) {
+        $this->layout = false;
+        $this->render('view', array(
+            'model' => $this->loadModel($id),
+        ));
+    }
+
+    /**
+     * Creates a new model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     */
+    public function actionSingle() {
+        $this->layout = false;
+        //$this->layout = '//layouts/popup';
+        $model = new Transaction;
+
+        // Uncomment the following line if AJAX validation is needed
+        $this->performAjaxValidation($model);
+        if (Yii::app()->request->isAjaxRequest) {
+            if (isset($_POST['Transaction'])) {
+                $model->attributes = $_POST['Transaction'];
+                $model->user = Yii::app()->user->id;
+                if (!empty($model->attributes['tag'])) {
+                    $model->tag = implode(",", (array) $model->attributes['tag']);
+                }
+                if ($model->attributes['transaction_type'] == 3) {
+                    $accounts = $model->account . ',' . $model->AccountTo;
+                    $model->account = implode(",", (array) $accounts);
+                }
+                if ($model->save()) {
+                    Yii::app()->user->setFlash('success', 'Transaction was saved successfully.');
+                    Yii::app()->end();
+                }
+            }
+            echo $this->renderPartial('single', array('model' => $model), true, true);
+            Yii::app()->end();
+        }
+
+        if (isset($_POST['Transaction'])) {
+            $model->attributes = $_POST['Transaction'];
+            $model->user = Yii::app()->user->id;
+            if (!empty($model->attributes['tag'])) {
+                $model->tag = implode(",", (array) $model->attributes['tag']);
+            }
+            if ($model->attributes['transaction_type'] == 3) {
+                $accounts = $model->account . ',' . $model->AccountTo;
+                $model->account = implode(",", (array) $accounts);
+            }
+            if ($model->save()) {
+                Yii::app()->user->setFlash('success', 'Transaction was saved successfully.');
+                $this->redirect(array('admin'));
+            }
+        }
+
+        $this->render('single', array(
+            'model' => $model,
+        ));
+    }
+
+    public function actionMultiple() {
+        $this->layout = false;
+        $model = new Transaction;
+
+        // Uncomment the following line if AJAX validation is needed
+        $this->performAjaxValidation($model);
+
+        if (Yii::app()->request->isAjaxRequest) {
+            if (isset($_POST['Transaction'])) {
+                $model = new Transaction;
+                for ($i = 1; $i <= 10; $i++) {
+                    $model->attributes = $_POST['Transaction'][$i];
+                    if (!empty($model->description) && !empty($model->amount) && !empty($model->account)) {
+                        if (!empty($model->attributes['tag'])) {
+                            $model->tag = implode(",", (array) $model->attributes['tag']);
+                        }
+                        $model->user = Yii::app()->user->id;
+                        $model->save();
+                        $model = new Transaction;
+                    }
+                }
+                Yii::app()->user->setFlash('success', 'Transactions was saved successfully.');
+                Yii::app()->end();
+            }
+        }
+
+        if (isset($_POST['Transaction'])) {
+            $model = new Transaction;
+            for ($i = 1; $i <= 10; $i++) {
+                $model->attributes = $_POST['Transaction'][$i];
+                if (!empty($model->description) && !empty($model->amount) && !empty($model->account)) {
+                    if (!empty($model->attributes['tag'])) {
+                        $model->tag = implode(",", (array) $model->attributes['tag']);
+                    }
+                    $model->user = Yii::app()->user->id;
+                    $model->save();
+                    $model = new Transaction;
+                }
+            }
+            Yii::app()->user->setFlash('success', 'Transactions was saved successfully.');
+            $this->redirect(array('admin'));
+        }
+
+        $this->render('multiple', array(
+            'model' => $model,
+        ));
+    }
+
+    /**
+     * Updates a particular model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id the ID of the model to be updated
+     */
+    public function actionUpdate($id) {
+        $this->layout = false;
+        $model = $this->loadModel($id);
+
+        // Uncomment the following line if AJAX validation is needed
+        $this->performAjaxValidation($model);
+
+        if (Yii::app()->request->isAjaxRequest) {
+            if (isset($_POST['Transaction'])) {
+                $model->attributes = $_POST['Transaction'];
+                $model->modified = new CDbExpression('NOW()');
+                if (!empty($model->attributes['tag'])) {
+                    $model->tag = implode(",", (array) $model->attributes['tag']);
+                }
+                if ($model->attributes['transaction_type'] == 3) {
+                    $accounts = $model->account . ',' . $model->AccountTo;
+                    $model->account = implode(",", (array) $accounts);
+                }
+                if ($model->save()) {
+                    Yii::app()->user->setFlash('success', 'Transactions was saved successfully.');
+                    Yii::app()->end();
+                }
+            }
+            if (isset($model->tag)) {
+                $model->tag = explode(',', $model->tag);
+            }
+            echo $this->renderPartial('update', array('model' => $model), true, true);
+            Yii::app()->end();
+        }
+
+        if (isset($_POST['Transaction'])) {
+            $model->attributes = $_POST['Transaction'];
+            $model->modified = new CDbExpression('NOW()');
+            if (!empty($model->attributes['tag'])) {
+                $model->tag = implode(",", (array) $model->attributes['tag']);
+            }
+            if ($model->attributes['transaction_type'] == 3) {
+                $accounts = $model->account . ',' . $model->AccountTo;
+                $model->account = implode(",", (array) $accounts);
+            }
+            if ($model->save()) {
+                Yii::app()->user->setFlash('success', 'Transactions was saved successfully.');
+                $this->redirect(array('admin'));
+            }
+        }
+
+        if (isset($model->tag)) {
+            $model->tag = explode(',', $model->tag);
+        }
+
+        $this->render('update', array(
+            'model' => $model,
+        ));
+    }
+
+    /**
+     * Deletes a particular model.
+     * If deletion is successful, the browser will be redirected to the 'admin' page.
+     * @param integer $id the ID of the model to be deleted
+     */
+    public function actionDelete($id) {
+        $this->loadModel($id)->delete();
+
+        // if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
+        if (!isset($_GET['ajax']))
+            $this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
+    }
+
+    /**
+     * Lists all models.
+     */
+    public function actionIndex() {
+        $this->redirect(array('admin'));
+        $dataProvider = new CActiveDataProvider('Transaction');
+        $this->render('index', array(
+            'dataProvider' => $dataProvider,
+        ));
+    }
+
+    /**
+     * Manages all models.
+     */
+    public function actionAdmin() {
+        $model = new Transaction('search');
+        $model->unsetAttributes();  // clear any default values
+        if (isset($_GET['Transaction']))
+            $model->attributes = $_GET['Transaction'];
+
+        $this->render('admin', array(
+            'model' => $model,
+        ));
+    }
+
+    /**
+     * Returns the data model based on the primary key given in the GET variable.
+     * If the data model is not found, an HTTP exception will be raised.
+     * @param integer $id the ID of the model to be loaded
+     * @return Transaction the loaded model
+     * @throws CHttpException
+     */
+    public function loadModel($id) {
+        $model = Transaction::model()->findByPk($id);
+        if ($model === null)
+            throw new CHttpException(404, 'The requested page does not exist.');
+        return $model;
+    }
+
+    /**
+     * Performs the AJAX validation.
+     * @param Transaction $model the model to be validated
+     */
+    protected function performAjaxValidation($model) {
+        if (isset($_POST['ajax']) && $_POST['ajax'] === 'transaction-form') {
+            echo CActiveForm::validate($model);
+            Yii::app()->end();
+        }
+    }
+
+}
